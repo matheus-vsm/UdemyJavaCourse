da54b27ce8f8984775783b637c2138729d527c32
Matheus <scompinha@hotmail.com>
2026-09-23 00:09:28 -0300

    aula 50

---
+++ src/secao_6_estruturas_repetitivas/aula_52_estrutura_repetitiva_enquanto_while/EstruturaRepetitivaEnquantoWhile.java
@@ -0,0 +1,7 @@
+package secao_6_estruturas_repetitivas.aula_52_estrutura_repetitiva_enquanto_while;
+
+public class EstruturaRepetitivaEnquantoWhile {
+    public static void main(String[] args) {
+
+    }
+}
