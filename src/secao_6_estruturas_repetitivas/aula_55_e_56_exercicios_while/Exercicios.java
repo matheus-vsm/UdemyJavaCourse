aa0ac1ca9c37c3f8e71a6d2ac7e360ec5a8c11eb
Matheus <scompinha@hotmail.com>
2026-09-23 20:21:10 -0300

    aulas 60 e 61 fazer

--- src/secao_6_estruturas_repetitivas/aula_55_e_56_exercicios_while/Exercicios.java
+++ src/secao_6_estruturas_repetitivas/aula_55_e_56_exercicios_while/Exercicios.java
@@ -1,4 +1,4 @@
-package secao_6_estruturas_repetitivas.aula_55_e_56_exercicios;
+package secao_6_estruturas_repetitivas.aula_55_e_56_exercicios_while;
 
 public class Exercicios {
     public static void main(String[] args) {
