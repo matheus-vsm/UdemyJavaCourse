0beb7c59ee7615f8feb13653b2191b6bc7f63b85
Matheus <scompinha@hotmail.com>
2026-09-23 00:00:15 -0300

    aula 46

--- src/secao_5_estrutura_condicional/aula_45_operadores_de_atribuicao_cumulativa/OperadoresDeAtribuicaoCumulativa.java
+++ src/secao_5_estrutura_condicional/aula_45_operadores_de_atribuicao_cumulativa/OperadoresDeAtribuicaoCumulativa.java
@@ -9,6 +9,7 @@ dá direito a 100 minutos de telefone. Cada minuto que exceder a
 franquia de 100 minutos custa R$2.00. Fazer um programa para ler a
 quantidade de minutos que uma pessoa consumiu,daí mostrar o valor
 a ser pago.
+
 ENTRADA      SAIDA
 22           Valor a pagar: R$50.00
 103          Valor a pagar: R$56.00
---
+++ src/secao_5_estrutura_condicional/aula_46_switch_case/SwitchCase.java
@@ -0,0 +1,52 @@
+package secao_5_estrutura_condicional.aula_46_switch_case;
+
+import java.util.Scanner;
+
+/*
+Fazer um programa para ler um valor inteiro de 1 a 7 representando um
+dia da semana (sendo 1  domingo, 2 = segunda, e assim por diante).
+Escrever na tela o dia da semana correspondente, conforme exemplos.
+
+ENTRADA          SAIDA
+1                Dia da semana: domingo
+4                Dia da semana: quarta
+9                Dia da semana: valor inválido
+*/
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        String dia;
+
+        switch (x) {
+            case 1:
+                dia = "domingo";
+                break;
+            case 2:
+                dia = "segunda";
+                break;
+            case 3:
+                dia = "terca";
+                break;
+            case 4:
+                dia = "quarta";
+                break;
+            case 5:
+                dia = "quinta";
+                break;
+            case 6:
+                dia = "sexta";
+                break;
+            case 7:
+                dia = "sabado";
+                break;
+            default:
+                dia = "valor invalido";
+                break;
+        }
+
+        System.out.println("Dia da semana: " + dia);
+        sc.close();
+    }
+}
