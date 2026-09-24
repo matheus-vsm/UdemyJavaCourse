7dc0ff2a39b873b8434982501d203d121007da34
Matheus <scompinha@hotmail.com>
2026-09-23 23:34:58 -0300

    aula 75

---
+++ src/secao_8_introducao_a_programacao_orientada_a_objetos/aula_75_object_e_tostring/application/ObjectEtoString.java
@@ -0,0 +1,57 @@
+package secao_8_introducao_a_programacao_orientada_a_objetos.aula_75_object_e_tostring.application;
+
+/*
+Fazer um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque). Em seguida:
+- Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no estoque)
+- Realizar uma entrada no estoque e mostrar novamente os dados do produto
+- Realizar uma saída no estoque e mostrar novamente os dados do produto
+Para resolver este problema, você deve criar uma CLASSE conforme projeto ao lado:
+Product
+- Name : string
+- Price : double
+- Quantity : int
++ TotalValueInStock() : double
++ AddProducts(quantity : int) : void
++ RemoveProducts(quantity : int) : void
+
+EXEMPLO
+Enter product data:
+Name: TV
+Price: 900.00
+Quantity in stock: 10
+
+Product data: TV, $ 900.00, 10 units, Total: $ 9000.00
+
+Enter the number of products to be added in stock: 5
+
+Updated data: TV, $ 900.00, 15 units, Total: $ 13500.00
+
+Enter the number of products to be removed from stock: 3
+
+Updated data: TV, $ 900.00, 12 units, Total: $ 10800.00
+*/
+
+import secao_8_introducao_a_programacao_orientada_a_objetos.aula_75_object_e_tostring.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ObjectEtoString {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println("\n" + product);
+
+        sc.close();
+    }
+}
